a7628f989b5203c6a44b4c45371687b884951e18
Oliver Juss <oliverjuss007@gmail.com>
2023-12-15 19:15:46 +0200

    add Week 14 code

---
+++ Week14OOP/Program.cs
@@ -0,0 +1,57 @@
+﻿Console.WriteLine("Name your Dog: ");
+string myDogName = Console.ReadLine();
+Dog myDog = new Dog(myDogName);
+Dog neighboursDog = new Dog("Good Girl");
+
+Console.WriteLine($"My dogs name is {myDog.Name}");
+Console.WriteLine($"My naghbors dos name is {neighboursDog.Name}");
+
+myDog.Rename("Bad Boy");
+
+while(myDog.LevelOfHapiness != 5)
+{
+    myDog.Bark();
+}
+
+myDog.WagTail();
+
+class Dog
+{
+    private string _name;
+    private int _levelOfHapiness;
+
+
+    public Dog(string name)
+    {
+        _name = name;
+        _levelOfHapiness = 0;
+    }
+
+    public string Name
+    {
+        get { return _name; }
+    }
+
+
+    public void Rename(string NewName)
+    {
+        _name = NewName;
+        Console.WriteLine($"The dog has been renamed to: {NewName}");
+    }
+
+
+    public void Bark()
+    {
+        Console.WriteLine("Woof-woof");
+        _levelOfHapiness++;
+    }
+    public int LevelOfHapiness
+    {
+        get { return _levelOfHapiness; }
+    }
+
+    public void WagTail()
+    {
+        Console.WriteLine("Wiggle-wiggle");
+    }
+}
